faaad643e27b12cf3b3ca95c5c13dcfc12ac291d
nhattienn12902 <nhattienn12902@gmail.com>
2026-08-01 17:17:20 +0700

    ci: add test profile config for JWT and S3 properties

--- src/test/java/com/nhattienn/ecommerce/EcommerceApiApplicationTests.java
+++ src/test/java/com/nhattienn/ecommerce/EcommerceApiApplicationTests.java
@@ -3,6 +3,7 @@ package com.nhattienn.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -12,6 +13,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Testcontainers
 class EcommerceApiApplicationTests {
 
